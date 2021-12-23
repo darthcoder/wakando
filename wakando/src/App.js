9951d8100db81884c63cab4b5302d5a939fbb758
Abdul Basit Ahmad <basit.005@gmail.com>
2021-12-23 14:10:24 +0530

    v0.1.0 ready
    working add remove and undo tasks capabilities

--- wakando/src/App.js
+++ wakando/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([]);
   const [task, setTask] = useState("");
 
-  const foo = () => {
+  const addTask = () => {
 
     let dataArray = [];
 
@@ -14,9 +14,35 @@ function App() {
     }
 
     dataArray[data.length] = task;
-    console.log(dataArray);
     setData(dataArray);
-    console.log(dataArray);
+    
+  }
+
+  const removeTask = () => {
+    let dataArray = [];
+
+    for(let i = 0; i < data.length; i++){
+      dataArray[i] = data[i];
+    }
+    
+    dataArray.pop();
+    setData(dataArray);
+
+  }
+
+  const undoTask = (j) => {
+    let dataArray = [];
+    
+    for (let i = 0; i < data.length ; i++){
+      if (i === j){
+        continue;
+      }
+      else {
+        dataArray[i] = data[i];
+      }
+    }
+
+    setData(dataArray);
   }
 
   // function addItem(item, d){
@@ -25,11 +51,12 @@ function App() {
   return (
     <div className="App">
       <ul>
-        {data.map((elem, i) =>(<li key={i}>{elem}</li>))}
+        {data.map((elem, j) =>(<li key={j}>{elem}<button onClick={() => {undoTask(j)}}>Undo Task</button></li>))}
       </ul>
     <input name="task" type="text" onChange={(event) => setTask(event.target.value)} 
     value={task}/>
-    <button onClick={foo}>Add Task</button>
+    <button onClick={addTask}>Add Task</button>
+    <button onClick={removeTask}>Remove Task</button>
     </div>
   );
 }
